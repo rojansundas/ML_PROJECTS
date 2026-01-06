e7bddde7379ef66f7459524d626cf3c2b9574590
rojansundas <sundasrojan@gmail.com>
2026-01-06 16:39:55 +0545

    New Project Tic Tac Toe

---
+++ ___init__.py
@@ -0,0 +1,5 @@
+from .assign_player import get_user,choice,board
+from .create_board import create_board
+from .fill_board import fill_board
+from .winner import winner
+
---
+++ fill_board.py
@@ -0,0 +1,4 @@
+def fill_board(board,player):
+  select=int(input("select 1,2,3,4,5,6,7,8,9"))
+  board[select]=player
+  return board
